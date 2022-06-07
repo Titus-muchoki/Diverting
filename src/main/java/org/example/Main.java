901c4082746ae278ff8058ac38ac0ea8ebc77573
kajela <titoyut56@gmail.com>
2022-06-07 01:56:05 -0400

    add the initial files

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,19 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String myObj = "the great women of africa ::";
+        String myObj1 = "the expensive pimp squad.";
+        int x = 23;
+        int y = 12;
+        int z = x +y;
+        String p = "2";
+        int q = 5;
+        String l = p + q;
+        String txt = "kajela is planning a meet up with \"tracy\" this friday";
+        System.out.println(myObj.concat(myObj1));
+        System.out.println(z);
+        System.out.println(l);
+        System.out.println(txt);
     }
 }
